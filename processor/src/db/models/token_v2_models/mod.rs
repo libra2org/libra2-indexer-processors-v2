e319b1ba6ae7e361e75b3799f42075ad444c8faa
yuunlimm <dla0267@gmail.com>
2025-01-29 11:13:30 -0800

    Migrate tokenV2 models and their dependencies (some of token)

--- processor/src/db/models/token_v2_models/mod.rs
+++ processor/src/db/models/token_v2_models/mod.rs
@@ -1,1 +1,7 @@
+pub mod token_claims;
+pub mod v1_token_royalty;
+pub mod v2_token_activities;
+pub mod v2_token_datas;
+pub mod v2_token_metadata;
+pub mod v2_token_ownerships;
 pub mod v2_token_utils;
