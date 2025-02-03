703f1c4328d03292b7b00c22562efb9aacc7acd3
yuunlimm <dla0267@gmail.com>
2025-02-03 15:04:00 -0800

    migrate integration crate

---
+++ integration-tests/src/lib.rs
@@ -0,0 +1,7 @@
+mod diff_test_helper;
+mod models;
+// mod sanity_test; // TODO: uncomment this once we have processors migrated to enable sanity tests
+mod sdk_tests;
+
+// const MAX_RETRIES: u32 = 3; // TODO: uncomment this once we have processors migrated and ready to run tests
+// const RETRY_DELAY: Duration = Duration::from_secs(2); // TODO: uncomment this once we have processors migrated and ready to run tests
